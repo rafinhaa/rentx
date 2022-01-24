c2952b4eb06ebd25443832324712d47c7485106d
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-24 12:14:03 -0300

    Chapter III - Consumindo API - Criação do projeto

---
+++ src/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { 
+  Container,
+  Title,
+} from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+}
+
+export default Home;
