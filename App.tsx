a6af4a884417c68cbbe0f57991696b58c7aa66e7
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-24 15:00:23 -0300

    Chapter III - Consumindo API - Tema Global da Aplicação

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import Home from './src/Home';
+import React from "react";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import { ThemeProvider } from "styled-components/native";
+
+import Home from "./src/Home";
+import AppLoading from "expo-app-loading";
+import theme from "./src/styles/theme";
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
-    <Home /> 
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -1,11 +1,9 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;  
+  flex: 1;
   justify-content: center;
   align-items: center;
 `;
 
-export const Title = styled.Text`
-
-`;
+export const Title = styled.Text``;
