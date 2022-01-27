15a89f98a2c5ca61dd40e27938cdfa242875cb3e
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-27 10:46:27 -0300

    Chapter III - Consumindo API - Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,8 +4,20 @@ import { RFValue } from "react-native-responsive-fontsize";
 import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
 import Logo from "../../assets/logo.svg";
+import Car from "../../components/Car";
 
 const Home: React.FC = () => {
+  const carData = {
+    brand: "Audi",
+    name: "RS 5 TS",
+    rent: {
+      period: "dia",
+      price: "1.500,00",
+    },
+    thumbnail:
+      "https://production.autoforce.com/uploads/version/profile_image/5505/model_main_comprar-prestige-plus-40-tfsi-s-tronic_7243435b0b.png",
+  };
+
   return (
     <Container>
       <StatusBar
@@ -19,6 +31,7 @@ const Home: React.FC = () => {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={carData} />
     </Container>
   );
 };
