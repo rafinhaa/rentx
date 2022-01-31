99db5cd32d509f2300d9b4ce2fc4bddc986c24b5
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-31 15:09:22 -0300

    Chapter III - Consumindo API - Componente de Acessórios

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import BackButton from "../../components/BackButton";
 import ImageSlider from "../../components/ImageSlider";
+import Accessory from "../../components/Accessory";
+
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
 
 import {
   Container,
@@ -15,6 +23,7 @@ import {
   Price,
   Period,
   About,
+  Accessories,
 } from "./styles";
 
 const CarDetails: React.FC = () => {
@@ -41,6 +50,14 @@ const CarDetails: React.FC = () => {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
         <About>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
           euismod, nisi vel consectetur egestas, nisl nisi consectetur tortor,
