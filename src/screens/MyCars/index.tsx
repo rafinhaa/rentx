88a49f5944023b1711f729f8dc4c3b384b85624f
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-02-08 10:24:12 -0300

    Chapter III - Consumindo API - Exibindo Carros Agendados

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,20 +1,58 @@
-import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
 import { Alert } from "react-native";
-import { CarDTO } from "src/dtos/CarDTO";
+import { StatusBar } from "expo-status-bar";
+import BackButton from "../../components/BackButton";
+import { CarDTO } from "../../dtos/CarDTO";
 import api from "../../services/api";
+import { useTheme } from "styled-components/native";
+import { AppRoutesParamList } from "../../routes/stack.routes";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import Load from "../../components/Load";
 
-import { Container } from "./styles";
+type MyCarsDetailsNavigationProps = NativeStackNavigationProp<
+  AppRoutesParamList,
+  "MyCars"
+>;
+
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarsList,
+} from "./styles";
+import Car from "../../components/Car";
+
+export interface MyCarsProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+}
 
 const MyCars: React.FC = () => {
-  const [cars, setCars] = React.useState<CarDTO>({} as CarDTO);
-  const [loading, setLoading] = React.useState(true);
+  const [cars, setCars] = useState<MyCarsProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  const navigation = useNavigation<MyCarsDetailsNavigationProps>();
+  const theme = useTheme();
+
+  function goBack() {
+    navigation.goBack();
+  }
 
   useEffect(() => {
     async function loadCars() {
       setLoading(true);
       try {
-        const response = await api.get("/schedules_byuser/?user_id=1");
-        setCars(response.data);
+        const { data } = await api.get<MyCarsProps[]>(
+          "/schedules_byuser/?user_id=1"
+        );
+        setCars(data);
+        console.log(cars);
       } catch (error) {
         Alert.alert("Erro ao carregar os carros");
         console.log(error);
@@ -24,7 +62,34 @@ const MyCars: React.FC = () => {
     }
     loadCars();
   }, []);
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header>
+        <StatusBar style="light" translucent backgroundColor="transparent" />
+        <BackButton onPress={goBack} color={theme.colors.shape} />
+        <Title>
+          Escolha uma {"\n"}
+          data de inicío {"\n"}e fim do aluguel
+        </Title>
+        <SubTitle>Conforto, segurança e praticidade</SubTitle>
+      </Header>
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>0</AppointmentsQuantity>
+        </Appointments>
+        {loading ? (
+          <Load />
+        ) : (
+          <CarsList
+            data={cars}
+            keyExtractor={(car) => String(car.id)}
+            renderItem={({ item }) => <Car data={item.car} />}
+          />
+        )}
+      </Content>
+    </Container>
+  );
 };
 
 export default MyCars;
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,3 +1,66 @@
+import { FlatList, FlatListProps } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { MyCarsProps } from "./";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 325px;
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: center;
+  padding: 25px;
+  padding-top: ${getStatusBarHeight() + 30}px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(30)}px;
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  margin-top: 24px;
+`;
+
+export const SubTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.secondary_400};
+  margin-top: 24px;
+`;
+
+export const Content = styled.View`
+  width: 100%;
+  flex: 1;
+  padding: 0 16px;
+`;
+
+export const Appointments = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px 0;
+`;
+
+export const AppointmentsTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+`;
+
+export const AppointmentsQuantity = styled.Text`
+  color: ${({ theme }) => theme.colors.title};
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+`;
+
+export const CarsList = styled(
+  FlatList as new (props: FlatListProps<MyCarsProps>) => FlatList<MyCarsProps>
+).attrs({
+  showVerticalScrollIndicator: false,
+})``;
