cd0300dcecbcfcae2f442a60c16927b625d3cd86
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-02-08 09:21:41 -0300

    Chapter III - Consumindo API - Listando Agendamentos do Usuário

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect } from "react";
+import { Alert } from "react-native";
+import { CarDTO } from "src/dtos/CarDTO";
+import api from "../../services/api";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 const MyCars: React.FC = () => {
-  return <View />;
+  const [cars, setCars] = React.useState<CarDTO>({} as CarDTO);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    async function loadCars() {
+      setLoading(true);
+      try {
+        const response = await api.get("/schedules_byuser/?user_id=1");
+        setCars(response.data);
+      } catch (error) {
+        Alert.alert("Erro ao carregar os carros");
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadCars();
+  }, []);
+  return <Container></Container>;
 };
 
 export default MyCars;
--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -74,6 +74,12 @@ const SchedulingDetails: React.FC = () => {
       ...schedulesByCar.data.unavailable_dates,
       ...dates,
     ];
+
+    await api.post("/schedules_byuser", {
+      user_id: 1,
+      car,
+    });
+
     api
       .put(`/schedules_bycars/${car.id}`, { id: car.id, unavailable_dates })
       .then(() => navigation.navigate("SchedulingComplete"))
