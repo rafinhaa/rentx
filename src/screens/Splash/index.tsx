d85a432962d5f8a3d679c9344e60bfde0849141c
Rafinhaa <rafinhaa@me.com>
2022-02-18 10:14:26 -0300

    Chapter III - Animações - Primeira Animação

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,9 +1,41 @@
 import React from "react";
 
+import { Button, StyleSheet } from "react-native";
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
 import { Container } from "./styles";
 
 const Splash: React.FC = () => {
-  return <Container />;
+  const animation = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: animation.value }],
+    };
+  });
+
+  function handleAnimatedPosition() {
+    animation.value++;
+  }
+
+  return (
+    <Container>
+      <Animated.View style={[styles.box, animatedStyle]} />
+      <Button title="Go to Home" onPress={handleAnimatedPosition} />
+    </Container>
+  );
 };
 
 export default Splash;
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+  },
+});
