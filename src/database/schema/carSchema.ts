a97144481a3ddd55a9aecfb5f2c4961049f2e2ce
Rafinhaa <rafinhaa@me.com>
2022-03-09 09:03:04 -0300

    Chapter IV - Autenticação e Navegação - Tabela de Carros

---
+++ src/database/schema/carSchema.ts
@@ -0,0 +1,37 @@
+import { TableSchema } from "@nozbe/watermelondb";
+
+const carSchema: TableSchema = {
+  name: "cars",
+  columns: [
+    {
+      name: "name",
+      type: "string",
+    },
+    {
+      name: "brand",
+      type: "string",
+    },
+    {
+      name: "about",
+      type: "string",
+    },
+    {
+      name: "fuel_type",
+      type: "string",
+    },
+    {
+      name: "period",
+      type: "string",
+    },
+    {
+      name: "price",
+      type: "number",
+    },
+    {
+      name: "thumbnail",
+      type: "string",
+    },
+  ],
+};
+
+export { carSchema };
